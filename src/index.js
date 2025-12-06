63aaa20f45e38e895be29e0bccdcbc7fea9e8b6e
anik-pas <anik-pas@ya.ru>
2025-12-06 09:53:39 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import './index.html';
 // new modules
 import './index.scss';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 
 
 // use modules
+
+const videoBG = document.querySelector('.video-bg');
+
+videoBG.innerHTML = `
+        <source src="video/video.webm" type="video/webm">
+        <source src="video/video.mp4" type="video/mp4">
+        `;
